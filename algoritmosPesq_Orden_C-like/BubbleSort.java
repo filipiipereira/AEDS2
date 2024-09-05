f4482063b00d95d5d682301dee7be9a9a3905785
Filipi Pereira <filipiacademico@gmail.com>
2024-09-05 13:34:10 -0300

    Adicionei BubbleSort

---
+++ algoritmosPesq_Orden_C-like/BubbleSort.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+
+
+class BubbleSort{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = {32,87,65,7,4,35,100,76,56};
+        for(int i = 0; i < array.length; i++){
+            for(int j = array.length - 1; j > 0; j--){
+                if(array[j] < array[j-1]) swap(array, j, j-1);
+            }
+        }
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i] + " ");
+        }
+        scanner.close();
+    }
+    private static void swap(int array[],int a, int b){
+        int temp = array[b];
+        array[b] = array[a];
+        array[a] = temp;
+    }
+}
