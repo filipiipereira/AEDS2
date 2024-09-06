8307110471b86a423eb22e75cac818b775c1e2af
Filipi Pereira <filipiacademico@gmail.com>
2024-09-06 20:50:41 -0300

    Adicionei Lista Sequencial

---
+++ TDA´s/Exercicio.java
@@ -0,0 +1,25 @@
+
+class Exercicio{
+    public static void main(String[] args) {
+      System.out.println("==== LISTA SEQUENCIAL ====");
+      Lista lista = new Lista(6);
+      int x1, x2, x3;
+
+      lista.InserirInicio(1);
+      lista.InserirFim(7);
+      lista.InserirFim(9);
+      lista.InserirInicio(3);
+      lista.Inserir(8, 3);
+      lista.Inserir(4, 2);
+
+      System.out.println("Apos insercoes: ");
+      lista.Mostrar();
+
+      x1 = lista.RemoverInicio();
+      x2 = lista.RemoverFim();
+      x3 = lista.Remover(2);
+
+      System.out.println("Apos remocoes (" + x1 + ", " + x2 + ", " + x3 + "):");
+      lista.Mostrar();
+    }
+}
---
+++ TDA´s/Lista.java
@@ -0,0 +1,59 @@
+
+
+
+
+public class Lista{
+    private int[] array;
+    private int contador;
+    public Lista(int tamanho){
+        this.array = new int[6];
+        this.contador = 0;
+    }
+    public Lista(){
+        this.array = new int[10];
+        this.contador = 0;
+    }
+    public void Inserir(int a, int posicao){
+        if(posicao >= 0 && posicao <= this.contador){
+            for(int i = this.contador; i > posicao; i--){
+                this.array[i] = this.array[i - 1];
+            }
+            this.array[posicao] = a;
+            this.contador++;
+        }
+    }
+    public void InserirInicio(int a){
+        this.Inserir(a, 0);
+    }
+    public void InserirFim(int a){
+        this.Inserir(a, this.contador);
+    }
+    public void Mostrar(){
+        if(contador != 0){
+            for(int i = 0; i < contador; i++){
+                System.out.print(this.array[i] + " ");
+            }
+        }
+    }
+    public int Remover(int posicao){
+        int valor;
+        if(posicao <= this.contador - 1){
+            valor = this.array[posicao];
+            for(int i = posicao; i < this.contador - 1; i++){
+                this.array[i] = this.array[i + 1];
+            }
+            this.contador--; 
+        }
+        else{
+            System.out.println("Nao ha elementos");
+            valor = -200;
+        }
+        return valor;
+    }
+    public int RemoverInicio(){
+        return this.Remover(0);
+    }
+    public int RemoverFim(){
+        return this.Remover(contador - 1);
+    }
+}
