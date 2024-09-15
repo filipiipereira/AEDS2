62fc55d7218b310295591e7318f940bcb535d668
Filipi Pereira <filipiacademico@gmail.com>
2024-09-15 11:18:13 -0300

    Ataulizei beecrowd desimpilhando caixas

--- beeCrowd/Exercicio1438.java
+++ beeCrowd/Exercicio1438.java
@@ -9,6 +9,7 @@ class Exercicio1438{
         int[][] pilhas = new int[quantidadeCaixas][quantidadePilhas];
         int numeroCaixasnaPilha;
         int quantidadeRetiradas;
+        int iCaixa1 = 0;
         while(quantidadeCaixas != 0 || quantidadePilhas != 0){
             for(int j = 0; j < quantidadePilhas; j++){
                 numeroCaixasnaPilha = scanner.nextInt();
@@ -19,13 +20,24 @@ class Exercicio1438{
                     pilhas[i][j] = 0;
                 }
             }
+            boolean encontrou = false;
+            for(int i = 0; i < quantidadeCaixas && !encontrou; i++){
+                for(int j = 0; j < quantidadePilhas && !encontrou ; j++){
+                    if(pilhas[i][j] == 1){
+                        encontrou = true;
+                        iCaixa1 = i;
+                    }
+                }
+            }
             quantidadeRetiradas = 0;
             int i = quantidadeCaixas - 1;
             int j = 0;
-            boolean encontrou = false;
+            encontrou = false;
             while(!encontrou){
-                while(i >= 0 && !encontrou){
-                    if(pilhas[i][j] == 1) encontrou = true;
+                while(i >= iCaixa1 && !encontrou){
+                    if(pilhas[i][j] == 1){
+                        encontrou = true;    
+                    }
                     if(pilhas[i][j] != 0 && !encontrou) quantidadeRetiradas++;
                     i--;
                 }
@@ -33,14 +45,14 @@ class Exercicio1438{
                 i = quantidadeCaixas - 1;
             }
             if(quantidadeRetiradas > quantidadeCaixas / 2){
-                quantidadeRetiradas = 0;
+                int quantidadeRetiradasDir = 0;
                 i = quantidadeCaixas - 1;
                 j = quantidadePilhas - 1;
                 encontrou = false;
                 while(!encontrou){
                     while(i >= 0 && !encontrou){
                         if(pilhas[i][j] == 1) encontrou = true;
-                        if(pilhas[i][j] != 0 && !encontrou) quantidadeRetiradas++;
+                        if(pilhas[i][j] != 0 && !encontrou) quantidadeRetiradasDir++;
                         i--;
                     }
                     j--;
